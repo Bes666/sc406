89c29ab9198b8f71d0e3fff2b9813872e6391ec3
Viste <viste02@gmail.com>
2011-04-13 00:26:46 +0800

    fix compile
    Signed-off-by: Viste <viste02@gmail.com>

--- src/server/game/Chat/Commands/Level3.cpp
+++ src/server/game/Chat/Commands/Level3.cpp
@@ -6226,13 +6226,13 @@ bool ChatHandler::HandleGMFlyCommand(const char *args)
             data << uint16(SMSG_MOVE_UNSET_CAN_FLY);
         else
         {
-            handler->SendSysMessage(LANG_USE_BOL);
+            SendSysMessage(LANG_USE_BOL);
             return false;
         }
         data.append(target->GetPackGUID());
         data << uint32(0);                                      // unknown
         target->SendMessageToSet(&data, true);
-        handler->PSendSysMessage(LANG_COMMAND_FLYMODE_STATUS, handler->GetNameLink(target).c_str(), args);
+        PSendSysMessage(LANG_COMMAND_FLYMODE_STATUS, handler->GetNameLink(target).c_str(), args);
         return true;
 }
 
