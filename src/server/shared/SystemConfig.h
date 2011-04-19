8551b1368455446255227508d53519663c716d41
Bes666 <mr-fear@ya.ru>
2011-04-19 14:14:32 -0700

    Edited src/server/shared/SystemConfig.h via GitHub

--- src/server/shared/SystemConfig.h
+++ src/server/shared/SystemConfig.h
@@ -28,7 +28,7 @@
 #include "Define.h"
 #include "revision.h"
 
-#define _PACKAGENAME "SkyFireEMU "
+#define _PACKAGENAME "WoWemu "
 #define _CODENAME "COLD_DAY_IN_HELL"
 
 #if TRINITY_ENDIAN == TRINITY_BIGENDIAN
